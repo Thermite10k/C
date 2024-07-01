17e526aca493b9fa9c1e361fd270c2d8e5ae5ff7
Ali batashi <batashi.ali@gmail.com>
2024-07-01 21:28:43 +0330

    added the funtion to chech if the rect is in correct format

--- book/c6/structandfunct.c
+++ book/c6/structandfunct.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #define XMAX 100
 #define YMAX 100
-
+#define MIN(a, b) ((a < b) ? (a) : (b))
+#define MAX(a, b) ((a > b) ? (a) : (b))
 
 struct point {
     int x;
@@ -14,7 +15,7 @@ struct rect {
 struct  point makepoint(int x, int y);
 struct point addpoint(struct point pt1, struct point pt2);
 int ptinbounds(struct rect rec, struct point p);
-
+struct rect canonrect(struct rect r);
 int main(){
 
    struct rect screen;
@@ -43,6 +44,23 @@ int main(){
     printf("%d\n", ptinbounds(screen, out));
     printf("%d\n", p4.x);
 
+    struct rect notcanonical = {
+        .pt1 = makepoint(10, 50),
+        .pt2 = makepoint(25, 20)
+    };
+
+    printf("%d\n", notcanonical.pt1.x);
+    printf("%d\n", notcanonical.pt1.y);
+    printf("%d\n", notcanonical.pt2.x);
+    printf("%d\n", notcanonical.pt2.y);
+    notcanonical = canonrect(notcanonical);
+
+    printf("%d\n", notcanonical.pt1.x);
+    printf("%d\n", notcanonical.pt1.y);
+    printf("%d\n", notcanonical.pt2.x);
+    printf("%d\n", notcanonical.pt2.y);
+    
+
     return 0;
 }
 
@@ -65,4 +83,33 @@ int ptinbounds(struct rect rec, struct point p){
         ((p.x >= rec.pt1.x)&&(p.x < rec.pt2.x)) && 
         ((p.y >= rec.pt1.y)&&(p.y <= rec.pt2.y))
         );
+}
+/* 
+    the ptinbounds assumes that the rect is in the standard or canonical form, that is 
+    the coordinates of rect.pt1 < rect.pt2.
+    let's write a function 
+*/
+
+struct rect canonrect(struct rect r){
+
+    // if(!(r.pt1.x < r.pt2.x)  || !(r.pt1.y < r.pt2.y)){    
+    //     //struct point temppt = {.x = r.pt1.x , .y = r.pt1.y}
+    //     struct point temppt = makepoint(r.pt1.x, r.pt1.y);
+    //     r.pt1 = r.pt2;
+    //     r.pt2 = temppt;
+    // }
+    // return r;
+    // this approcah does not consider the condition where only x or y is incorrect, it can be fixed with more if/elses but 
+    // I'll use the approach from the book as it's more concise, offers brevity.
+    
+    struct rect temprect = {
+        .pt1.x = MIN(r.pt1.x, r.pt2.x),
+        .pt1.y = MIN(r.pt1.y, r.pt2.y),
+        .pt2.x = MAX(r.pt1.x, r.pt2.x),
+        .pt2.y = MAX(r.pt1.y, r.pt2.y)
+    };
+
+    return temprect;
+
+
 }
