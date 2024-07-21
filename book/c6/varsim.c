4e229884ba43ec9f431275006eb523333fdf7358
Ali batashi <batashi.ali@gmail.com>
2024-07-21 23:20:17 +0330

    Implemented the base functions

--- book/c6/varsim.c
+++ book/c6/varsim.c
@@ -28,7 +28,60 @@ int main(int argc, char *argv[]){
         }
     }
 
+    char word[MAXWORD];
+
+    while(getword(word, MAXWORD) != EOF){
+        printf("%s\n", word);
+    }
+
     printf("Val is: %d\n", val);
 
     return 0;
 }
+
+int getword(char *word, int limit){
+
+    int c;
+    
+    while(isspace(c = getch())){
+        ;
+    }
+    char *w = word;
+    if(c != EOF){
+        *w++ = c; 
+    }
+    if(!isalnum(c)){
+        *w = '\0';
+        return c;
+    }
+    for(; --limit; w++){
+        if(!isalnum(*w = getch())){
+            ungetch(*w);
+            break;
+        }
+    }
+    *w = '\0';
+    return word[0];
+
+}
+
+#define BUFFSIZE 100
+
+char buffer[BUFFSIZE];
+int bufferindex = 0;
+
+char getch(void){
+    if(bufferindex > 0){
+        return buffer[--bufferindex];
+    }else{
+        return getchar();
+    }
+}
+void ungetch(char c){
+    if(bufferindex < BUFFSIZE){
+        buffer[bufferindex++] = c;
+    }else{
+        printf("error: ungetch: buffer is full.");
+    }
+}
+
