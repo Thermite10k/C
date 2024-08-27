b7835d6eb7888a0e3baf7efe6d1e35377bd8a376
Ali batashi <batashi.ali@gmail.com>
2024-08-27 18:24:47 +0330

    Reads the puzzle input and saves it in memory as a 2D array

--- adventofcode/3/3.c
+++ adventofcode/3/3.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <string.h>
 #define MAXWORD 100
 #define CHARBUFF 10
 #define MAXLINE 1000
+#define MAX(a,b) ((a > b) ? a : b)
 
 //int getword(int limit, char *word, FILE *fp);
 //char getfilechar(FILE *fp);
@@ -13,12 +15,38 @@ int getfileline(int limit, FILE *fp, char *line);
 
 int main(void){
     char *line = (char *)malloc(MAXLINE); // char line[maxline] is also valid.
-    int length = 0;
+    int length = 0; // columns
+    int *ROWS = malloc(sizeof(int));
+    int *COLS = malloc(sizeof(int));
+    *COLS = 0;
+    *ROWS = 0;
+    char **puzzle = (char **)malloc(MAXLINE);
+    char **pp = puzzle;
     FILE *fp;
     fp = fopen("key.txt", "r");
     while((length = getfileline(MAXLINE, fp, line))){
-        printf("Line: \t%sLine: \t%d\n", line, length);
+        //printf("Line: \t%sLine: \t%d\n", line, length);
+        *pp = (char *)malloc(strlen(line) + 1);
+        memcpy(*pp, line, strlen(line) + 1);
+        *pp++;
+        (*ROWS)++;
+        *COLS = MAX(*COLS, length);
     }
+    printf("%d rows.\n", *ROWS);
+    printf("%d cols.\n", *COLS);
+
+    pp = puzzle;
+    int i = 0;
+
+    line = *pp;
+   
+
+        printf("%c", puzzle[9][1]);
+    
+
+    
+    
+    
     return 0;
 }
 //int getword(int limit, char *word);
