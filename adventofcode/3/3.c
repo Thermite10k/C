c4c95dde3945721fc2f9292eb5efac65d7071542
Ali batashi <batashi.ali@gmail.com>
2024-08-29 00:28:41 +0330

    added the ittereation function, now time to implement evaluation

--- adventofcode/3/3.c
+++ adventofcode/3/3.c
@@ -5,14 +5,16 @@
 #define MAXWORD 100
 #define CHARBUFF 10
 #define MAXLINE 1000
+#define MAXWORD 100
 #define MAX(a,b) ((a > b) ? a : b)
 
 //int getword(int limit, char *word, FILE *fp);
 //char getfilechar(FILE *fp);
 //void ungetfilechar(char c);
 int evaluate(char (*)[], int x, int y);
+int iteratepuzzle(char **puzzle, int rows, int cols);
 int getfileline(int limit, FILE *fp, char *line);
-
+int getwordfromline(char *line, char *word, int limit);
 int main(void){
     char *line = (char *)malloc(MAXLINE); // char line[maxline] is also valid.
     int length = 0; // columns
@@ -26,7 +28,7 @@ int main(void){
     fp = fopen("key.txt", "r");
     while((length = getfileline(MAXLINE, fp, line))){
         //printf("Line: \t%sLine: \t%d\n", line, length);
-        *pp = (char *)malloc(length);
+        *pp = (char *)malloc(length); //puzzle pointer
         memcpy(*pp, line, length);
         *pp++;
         (*ROWS)++;
@@ -34,19 +36,22 @@ int main(void){
     }
     printf("%d rows.\n", *ROWS);
     printf("%d cols.\n", *COLS);
+    char *myline = "This is a test...*string";
+    char *lineptr = myline;
+    char word[MAXWORD];
+    while(*lineptr != '\0'){
 
-    pp = puzzle;
-    int i = 0;
+        if(getwordfromline(lineptr, word, MAXWORD)){
+            printf("%s\n", word);
+            lineptr += strlen(word);
+            while(isspace(*lineptr)){
+                lineptr++;
+            }
+        }
+    }
 
-    line = *pp;
-   
+    //iteratepuzzle(puzzle, *ROWS, *COLS);
 
-        printf("%c", puzzle[9][9]);
-    
-
-    
-    
-    
     return 0;
 }
 //int getword(int limit, char *word);
@@ -67,4 +72,45 @@ int getfileline(int limit, FILE *fp, char *line){ // char *line[] is an array of
 
 
     return i;
+}
+
+int iteratepuzzle(char **puzzle, int rows, int cols){
+    int x = 0, y = 0;
+    char word[MAXWORD];
+    for(y=0; y<rows; y++){
+        // for(x=0; x<=cols; x++){
+        //     //printf("%c", puzzle[y][x]);
+        //     printf("%c", *(*(puzzle + y) + x));
+        // }
+        while(getwordfromline(*(puzzle + y), word, MAXWORD)){
+        printf("%s", word);
+            
+        }
+    }
+}
+int getwordfromline(char *line, char *word, int limit){
+
+    int c;
+    char *w = word;
+    while(isspace((c = *line++))){
+        ;
+    }
+    if(c != EOF){
+        *w++ = c;
+    }
+    if(!isalpha(c)){
+        *w = '\0';
+        return c;
+    }
+    for(; --limit > 0; line++, w++){
+        
+        if(!isalnum((*w = *line))){
+            line--;
+            break;
+        }
+    }
+
+    *w = '\0';
+    return word[0];
+
 }
