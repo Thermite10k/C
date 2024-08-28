5a7308a0b785c8b2d77782f73d3390f4b53907a7
Ali batashi <batashi.ali@gmail.com>
2024-08-29 00:58:42 +0330

    now the linepointer is better maintained. memcpy argumet fixed

--- adventofcode/3/3.c
+++ adventofcode/3/3.c
@@ -14,7 +14,7 @@
 int evaluate(char (*)[], int x, int y);
 int iteratepuzzle(char **puzzle, int rows, int cols);
 int getfileline(int limit, FILE *fp, char *line);
-int getwordfromline(char *line, char *word, int limit);
+int getwordfromline(char **line, char *word, int limit);
 int main(void){
     char *line = (char *)malloc(MAXLINE); // char line[maxline] is also valid.
     int length = 0; // columns
@@ -22,35 +22,23 @@ int main(void){
     int *COLS = malloc(sizeof(int));
     *COLS = 0;
     *ROWS = 0;
-    char **puzzle = (char **)malloc(MAXLINE);
+    char **puzzle = (char **)malloc(MAXLINE); // a 2-D array
     char **pp = puzzle;
     FILE *fp;
     fp = fopen("key.txt", "r");
     while((length = getfileline(MAXLINE, fp, line))){
         //printf("Line: \t%sLine: \t%d\n", line, length);
-        *pp = (char *)malloc(length); //puzzle pointer
-        memcpy(*pp, line, length);
+        *pp = (char *)malloc(length+1); //puzzle pointer allocates space for storing the n-th line.
+        memcpy(*pp, line, length+1);
         *pp++;
         (*ROWS)++;
         *COLS = MAX(*COLS, length-1);
     }
     printf("%d rows.\n", *ROWS);
     printf("%d cols.\n", *COLS);
-    char *myline = "This is a test...*string";
-    char *lineptr = myline;
-    char word[MAXWORD];
-    while(*lineptr != '\0'){
 
-        if(getwordfromline(lineptr, word, MAXWORD)){
-            printf("%s\n", word);
-            lineptr += strlen(word);
-            while(isspace(*lineptr)){
-                lineptr++;
-            }
-        }
-    }
 
-    //iteratepuzzle(puzzle, *ROWS, *COLS);
+    iteratepuzzle(puzzle, *ROWS, *COLS);
 
     return 0;
 }
@@ -61,7 +49,7 @@ int evaluate(char (*)[], int x, int y){
     return 0;
 }
 int getfileline(int limit, FILE *fp, char *line){ // char *line[] is an array of lines, not what we need.
-    int i, c;
+    int i=0, c;
     for(i=0; (i < limit - 1) && (c = getc(fp)) != '\n' && c != EOF; i++){
         line[i] = c;
     }
@@ -77,22 +65,28 @@ int getfileline(int limit, FILE *fp, char *line){ // char *line[] is an array of
 int iteratepuzzle(char **puzzle, int rows, int cols){
     int x = 0, y = 0;
     char word[MAXWORD];
+    char *lineptr;
     for(y=0; y<rows; y++){
         // for(x=0; x<=cols; x++){
         //     //printf("%c", puzzle[y][x]);
         //     printf("%c", *(*(puzzle + y) + x));
         // }
-        while(getwordfromline(*(puzzle + y), word, MAXWORD)){
-        printf("%s", word);
-            
-        }
+        
+        lineptr = *(puzzle + y);
+    
+            while(getwordfromline(&lineptr, word, MAXWORD)){
+                printf("%s", word);         
+            }
+        
+        printf("\n");
     }
 }
-int getwordfromline(char *line, char *word, int limit){
+int getwordfromline(char **line, char *word, int limit){
 
     int c;
     char *w = word;
-    while(isspace((c = *line++))){
+  
+    while(isspace((c = *(*line)++))){
         ;
     }
     if(c != EOF){
@@ -102,10 +96,10 @@ int getwordfromline(char *line, char *word, int limit){
         *w = '\0';
         return c;
     }
-    for(; --limit > 0; line++, w++){
+    for(; --limit > 0; (*line)++, w++){
         
-        if(!isalnum((*w = *line))){
-            line--;
+        if(!isalnum((*w = **line))){
+            (*line)--;
             break;
         }
     }
