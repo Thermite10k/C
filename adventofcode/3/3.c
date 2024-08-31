3beb0ec3a20adf7e5dc2ba9b357c49c74e8910bb
Ali batashi <batashi.ali@gmail.com>
2024-08-31 16:01:54 +0330

    it works!

--- adventofcode/3/3.c
+++ adventofcode/3/3.c
@@ -17,8 +17,8 @@ int myatoi(char *str);
 int check(char **puzzle, int x, int y, int rows, int cols, int length);
 int isvalidsymbol(char value);
 int getneighbordigit(char **puzzle, int x, int y, int rows, int cols);
-int getneighbornumber(char **puzzle, int x, int y);
-
+int getneighbornumber(char **puzzle, int x, int y, int *valueX);
+int getgerratio(char **puzzle, int x, int y, int rows, int cols);
 int main(void){
     char *line = (char *)malloc(MAXLINE); // char line[maxline] is also valid.
     int length = 0; // columns
@@ -101,7 +101,7 @@ int iteratepuzzle(char **puzzle, int rows, int cols){
                     }
                 }
                 if(*word == '*'){
-                    gearRatio += getneighbordigit(puzzle, x-length, y, rows, cols);
+                    gearRatio += getgerratio(puzzle, x-length, y, rows, cols);
                 }
         
             }
@@ -176,67 +176,75 @@ int isvalidsymbol(char value){
    // anything is a valid symbol as long as it's not alnum and it's not a period 
     return(!isalnum(value) && value != '.');
 }
-int getneighbordigit(char **puzzle, int x, int y, int rows, int cols){
-    int firstNumber = 1, secondNumber = 1, checkNumber = 1;
-    /*
-          23779   | Get a digiti and see what number it belongs too, get the number and save it.
-            *     | Get the second digit, if any, and store it in the second var. if you come across
-            46    | a new digit, get its number and if it's not the same as the second number, terminate.
-    */
-    if((x-1) >= 0){ // if anyhting is to the left (x-1), check it.
-        if(isdigit(puzzle[y][x-1])){
-            if(firstNumber == 1){
-                firstNumber = getneighbornumber(puzzle, x-1, y);
-              
-            }else{
-                checkNumber = getneighbornumber(puzzle, x-1, y);
-                if(secondNumber == 1){
-                    secondNumber = checkNumber;
-                }else if(secondNumber != checkNumber){
-                    return 0;
-                }
-            }
-        }
-    }
-    if(isdigit(puzzle[y][x])){// for upper and lower rows
-        if(firstNumber == 1){
-            firstNumber = getneighbornumber(puzzle, x, y);
-        }else{
-            checkNumber = getneighbornumber(puzzle, x, y);
-            if(secondNumber == 1){
-                secondNumber = checkNumber;
-            }else if(secondNumber != checkNumber){
-                return 0;
-            }
-        }
-    }
-    if((x+1) < cols){// checking the right side.
-        if(isdigit(puzzle[y][x+1])){
-            if(firstNumber == 1){
-                firstNumber = getneighbornumber(puzzle, x+1, y);
-            }else{
-                checkNumber = getneighbornumber(puzzle, x+1, y);
-                if(secondNumber == 1){
-                    secondNumber = checkNumber;
-                }else if(secondNumber != checkNumber){
-                    return 0;
-                }
+int getgerratio(char **puzzle, int x, int y, int rows, int cols){
 
+    struct numbercoordinates {
+        int value;
+        int x;
+        int y;
+        int hash;
+    };
+
+    // 9 pointers to structs, same as: struct numbercoordinates array[9].
+    // 9 is how many structs we might have at most as * has 9 neighbors including itself.
+    struct numbercoordinates **coordinatesarry = malloc(9 * sizeof(struct numbercoordinates));
+
+    // coordinatesarry[0] = malloc(sizeof(struct numbercoordinates));
+    // coordinatesarry[0]->value = 250;
+
+
+    int dy = -1, dx = -1, i = 0, value = 0, j=0, valueX = 0, hash1 = -1, hash2 = -1;
+    int firstNumber = 0, secondNumber = 0;
+    int **numbers = (int **)malloc((9 * sizeof(int *)));
+
+    for(dy = -1; dy < 2; dy++){
+        if((y+dy) >= 0 && (y+dy) < rows){
+            for(dx = -1; dx < 2; dx++){
+                if((x + dx) >= 0 && (x + dx) < cols){
+                    if(isdigit(puzzle[y+dy][x+dx])){
+                        valueX = 0;
+                        value = getneighbornumber(puzzle, x+dx, y+dy, &valueX);
+                        *(coordinatesarry + i) = malloc(sizeof(struct numbercoordinates));
+                        (*(coordinatesarry + i))->x = valueX;
+                        (*(coordinatesarry + i))->y = y+dy;
+                        (*(coordinatesarry + i))->value = value;
+                        (*(coordinatesarry + i))->hash = 4*valueX + 3 *(y+dy);
+                        i++;
+                    }
+                }
             }
         }
     }
-    
+
+    for(j=0; j < i; j++){
+        struct numbercoordinates *number = coordinatesarry[j];
+        if(hash1 == -1){
+            hash1 = number->hash;
+            firstNumber = number->value;
+        }else if(number->hash != hash1 && hash2 == -1){// if it's a new number other than the first one
+            hash2 = number->hash;
+            secondNumber = number->value;
+        }else if(hash2 != -1 && number->hash != hash2){// if second number is present and a new number is received
+            secondNumber = 0;
+        }
+    }
+
     return firstNumber * secondNumber;
+    // free the memory.
 }
-int getneighbornumber(char **puzzle, int x, int y){
+
+int getneighbornumber(char **puzzle, int x, int y, int *valueX){
     int number = 0;
     char *line = *(puzzle + y);
     char *n_ptr = line+x; // number pointer
 
     while(isdigit(*n_ptr)){// go to the begining of the number.
+        x--;
         n_ptr--;
     }
+    x++;
     n_ptr++; // now set the pointer to the first digit of the number
+    *valueX = x;
     while(isdigit(*n_ptr)){
         number = 10 * number + *n_ptr++ - '0';
     }
