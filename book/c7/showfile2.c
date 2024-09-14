a6316f75ead6906196e2fbe9c37b5c2c4521eb91
Ali batashi <batashi.ali@gmail.com>
2024-09-14 18:25:34 +0330

    modified to show .jpg files

--- book/c7/showfile2.c
+++ book/c7/showfile2.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]){
         filecopy(stdin, stdout);
     }else{
         while(--argc > 0){
-            if((fp = fopen(*++argv, "r")) == NULL){
+            if((fp = fopen(*++argv, "rb")) == NULL){
                 fprintf(stderr, "%s: can't open %s\n", prog, *argv);
                 exit(1);
             } else {
@@ -20,7 +20,7 @@ int main(int argc, char *argv[]){
             }
         }
     }
-    if(ferror(stdout)){
+    if(ferror(stdout)){ // ferror (*stream) returns non zero if error occures on the stream.
         fprintf(stderr, "%s: error writing stdout\n", prog);
         exit(2);
     }
@@ -28,8 +28,8 @@ int main(int argc, char *argv[]){
 }
 void filecopy(FILE *in, FILE *out){
     int c;
-
     while((c = getc(in)) != EOF){
-        putc(c, out);
+        fprintf(out, " %d ", c);
+        //putc(c - 0, out);
     }
 }
