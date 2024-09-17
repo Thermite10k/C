884e20768f95d75ff40d13860eb7efc12b6aded9
Ali batashi <batashi.ali@gmail.com>
2024-09-17 17:47:32 +0330

    The issue was in the printf. In some places,  I had used %d not %lld

--- adventofcode/5/5.c
+++ adventofcode/5/5.c
@@ -11,28 +11,29 @@ char getfilechar(FILE *fp);
 void ungetfilechar(char c);
 int getfileline(FILE *fp, int limit, char *line);
 int getlineword(char **line, char *word, int limit);
-long updatesrc(long src, long from, long to, long range);
+ long long myatoi(char *word);
+ long long updatesrc( long long src,   long long from,   long long to,   long long range);
 
 int main(int argc, char *argv[]){
     int wordstatus, linestatus, i;
-    long dest = 0;
+     long long dest = 0;
     FILE *fp = fopen(KEYFILE, "r");
     char line[MAXLINE];
     char *lineptr;
     char word[MAXLINE];
-    long src[MAXSEEDS];
-    long updatedsrc[MAXSEEDS];
+     long long src[MAXSEEDS];
+     long long updatedsrc[MAXSEEDS];
     int srcindex = 0;
     char *newmap = "%[^-]-to-%[^ ] map:";
-    char *map = "%d%d%d";
+    char *map = "%lld%lld%lld";
     char *from[MAXLINE], *to[MAXLINE];
-    long rangeto, rangefrom, range;
+    long long int rangeto, rangefrom, range;
     /* First line contains the seeds */
     getfileline(fp, MAXLINE, line); // storing the seeds in line
     lineptr = line;
     while(getlineword(&lineptr, word, MAXLINE) != '\0'){
         if(srcindex < MAXSEEDS && isdigit(*word)){
-            printf("%lld ", (src[srcindex++] = atoi(word)));
+            printf("%lld ", (src[srcindex++] = myatoi(word)));
             updatedsrc[i] = 0;
         }
     }
@@ -47,14 +48,14 @@ int main(int argc, char *argv[]){
         if((scanstatus = sscanf(line, newmap, from, to)) == 2){
             //printf("%s to %s\n", from, to);
         }else if ((scanstatus = sscanf(line, map, &rangeto, &rangefrom, &range)) == 3){
-            //printf("Going from %d to %d with range: %d\n", rangefrom, rangeto, range);
+            printf("Going from %lld to %lld with range: %lld\n", rangefrom, rangeto, range);
             // while((wordstatus = getlineword(&lineptr, word, MAXLINE)) != EOF && wordstatus !='\0'){         
             // }
             for(i=0; (i < srcindex); i++){
                 if(src[i]){ // if src has not been updated, (we set it to zero if it's updated)
                     dest = updatesrc(src[i], rangefrom, rangeto, range);
                     if(dest){ // if it has a value within range
-                        //printf("%d goes to %d\n", src[i], dest);
+                        printf("%lld goes to %lld\n", src[i], dest);
                         updatedsrc[i] = dest;
                         src[i] = 0; // if we have an updated value, do not check src[i] again.
                     }
@@ -73,10 +74,12 @@ int main(int argc, char *argv[]){
         }
     }
 
-
+    long long min = src[0];
     for(i = 0; i<srcindex; i++){
-        printf("%d ", src[i]);
+        min = src[i] < min ? src[i] : min;
     }
+
+    printf("\n\t%lld", min);
     
 
     return 0;
@@ -141,12 +144,21 @@ int getlineword(char **line, char *word, int limit){
     *w = '\0';
     return word[0];
 }
-long updatesrc(long src, long from, long to, long range){
+ long long updatesrc( long long src,  long long from,  long long to,  long long range){
     long long step = to - from;
 
-    if(src >= from && src < from + range){
+    if(src >= from && src < (from + range)){
         return (src + step);
     }else{
         return 0;
     }
+}
+ long long myatoi(char *word){
+     long long number = 0;
+    while(*word){
+        number = 10 * number + *word - '0';
+        word++;
+    }
+
+    return number;
 }
