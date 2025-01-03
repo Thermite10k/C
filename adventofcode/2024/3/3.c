69d977c855554b97bc8fcc67a12340d69c60744a
Ali batashi <batashi.ali@gmail.com>
2025-01-04 01:44:50 +0330

    works on test input, not with the puzzle input

--- adventofcode/2024/3/3.c
+++ adventofcode/2024/3/3.c
@@ -1,27 +1,110 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define CHAR_BUFF_SIZE 10
 #define MAX_WORD 100
 #define KEY "key.txt"
+#define KEY_2 "key2.txt"
 #define READ_MODE "r"
+#define MAX_LINES 10
 
 int get_f_char(FILE* fp);
 void unget_f_char(char c);
 
 int get_f_word(FILE* fp, int limit, char* word);
 
+int str_in_str(char* s1, char* s2); // s1 in s2 ? 1 : 0;
+int str_end_in_str(char* s1, char* s2); // s1 ends in s2 ? 1 : 0;
+
+
+
 int main(){
     
     char word[MAX_WORD] ;
 
     FILE* fp;
-    fp = fopen(KEY, READ_MODE);
+    fp = fopen(KEY_2, READ_MODE);
+    int i = 0, max_prams = 5;
+    char prams[10][10];
+    char mulPattern[] = "(%d,%d)";
+    int j = 0; // the counter for mulPattern
+    char c; // used instead of mulPattern[j]
+    int firstNumber = 0, secondNumber = 0;
+    int patternLength = strlen(mulPattern);
 
-    while(get_f_word(fp, MAX_WORD, word) != EOF){
-        printf("%s\n", word);
+    unsigned long long answer = 0;
+    long long tempAnswer = 0;
+    long long totalAnswers[MAX_LINES];
+
+    for(int i2 = 0; i2 < MAX_LINES; i2++){
+        totalAnswers[i2] = 0;
     }
 
+    int status = 0;
+    int lines = 0;
+
+    //printf("Len is%d\n", patternLength);
+    while((status = get_f_word(fp, MAX_WORD, word)) != EOF){
+        if(status == '\n'){
+            lines++;
+            printf("___________________________________________________________\n");
+            continue;
+        }
+        i = 0;
+        if(str_end_in_str("mul", word)){
+            //printf("\t%s\n", word);
+            // if you see a mul, get max_prams next inputs
+            while(i < max_prams && get_f_word(fp, MAX_WORD, word) != EOF){
+                strcpy(prams[i++], word);
+            }
+            if(i == max_prams){
+                tempAnswer = 1;
+                i = 0;
+                j = 0;
+                while((c = mulPattern[j]) != '\0'){
+                    if(c == '%'){
+                        c = mulPattern[++j];
+                        switch(c){
+                            case 'd':
+                                tempAnswer *= atoi(prams[i++]);
+                                break;
+                            default:
+                                fprintf(stderr, "Unknown pattern: %c\n", c);
+                                break;
+                        }
+                    }else{
+                        if(mulPattern[j] == *prams[i]){
+                            i++;
+                        }else{
+                            
+                            break;
+                        }
+                    }
+                    j++;
+                }
+                if(j == patternLength){
+                    printf("mul");
+                    for(int k = 0; k < i; k++){
+                        printf("%s", prams[k]);
+                    }
+                    printf(" = %d in line %d\n", tempAnswer, lines);
+                    totalAnswers[lines] += tempAnswer;
+                    answer += tempAnswer;
+                }
+            }
+        }
+
+    }
+    printf("%lld\n in %d lines\n", answer, lines);
+
+    for(int l = 0; l < lines; l++){
+        if(totalAnswers[l]){
+            printf("%lld\n", totalAnswers[l]);
+        }
+    }
+    
     return 0;
 }
 
@@ -48,7 +131,8 @@ int get_f_word(FILE* fp, int limit, char* word){
     int c;
 
     c = get_f_char(fp);
-    if(c != EOF){
+
+    if(c != EOF && c != '\n'){
         *w++ = c;
     }
     if(!isalnum(c) || c == EOF){
@@ -66,4 +150,65 @@ int get_f_word(FILE* fp, int limit, char* word){
     *w = '\0';
 
     return *word;
+}
+
+int str_in_str(char* s1, char* s2){ // s1 in s2 ? 1 : 0;
+
+    char* str1 = s1;
+    char* str2 = s2;
+
+    int i = 0, j = 0;
+    if(*str1 == '\0' || *str2 == '\0'){
+        fprintf(stderr, "error: str_in_str: s1 or s2 has length of 0\n");
+    }
+
+    while(str2[i] != '\0'){
+        if(str1[j] == str2[i]){
+            j++;
+        }else{
+            j = 0;
+        }
+        if(str1[j] == '\0'){
+            return 1;
+        }
+        i++;
+    }
+
+    return 0;
+
+}
+
+int str_end_in_str(char* s1, char* s2){ // s2 ends in s1 ? 1 : 0;
+
+    char* str1 = s1;
+    char* str2 = s2;
+
+    int i = 0, j = 0;
+    int lenOfS1 = 0;
+
+    while(str2[i]){
+        i++;
+    }
+    while(str1[j]){
+        j++;
+    }
+    if(i == 0 || j == 0){
+        fprintf(stderr, "error: str_end_in_str: s1 or s2 has length of 0\n");
+    }
+    j--;
+    i--; // to skip the '\0'
+    lenOfS1 = j;
+    while(i >= 0){
+        if(str2[i] == str1[j]){
+            j--;
+        }else{
+            j = lenOfS1;
+        }
+        if(j == 0){
+            return 1;
+        }
+        i--;
+    }
+
+    return 0;
 }
