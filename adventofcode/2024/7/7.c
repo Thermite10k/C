294bccdf0f0afd28921b0bf63fd825b9b8329d0f
Ali batashi <batashi.ali@gmail.com>
2025-03-31 16:52:48 +0330

    Added part I

--- adventofcode/2024/7/7.c
+++ adventofcode/2024/7/7.c
@@ -9,34 +9,40 @@
 
 char get_f_c(FILE *fp);
 void unget_f_c(char C);
-int32_t get_f_number(FILE *fp, int32_t limit, int32_t *number);
-
+int64_t get_f_number(FILE *fp, int32_t limit, int64_t *number);
+int32_t can_be_solved(int64_t acc, int32_t index, int64_t answer, int64_t *numbers);
 int main(){
 
-    int32_t numbersArray[MAX_NUMBERS];
+    int64_t numbersArray[MAX_NUMBERS];
     int32_t numbersArrayIndex = 0;
-    int32_t temp = 0;
+    int64_t temp = 0;
     int32_t status = 0;
+    uint64_t answer = 0;
     
-    FILE *fp = fopen(KEY_TEST, "r");
+    FILE *fp = fopen(KEY_MAIN, "r");
 
     while ((status = get_f_number(fp, MAX_NUMBERS, &temp)) != EOF){
-
-            fprintf(stdout, "%d: ", temp);
-   
+        numbersArrayIndex = 0;
+        numbersArray[numbersArrayIndex++] = temp;
         while ((status = get_f_number(fp, MAX_NUMBERS, &temp)) != EOF){
-            printf("%d ", temp);
+            numbersArray[numbersArrayIndex++] = temp;
             if(status == '\n'){
-             putchar('\n');
+            numbersArray[numbersArrayIndex] = -1; // this will marl the end of the current set.
+            for(uint32_t i = 0; i < numbersArrayIndex; i++){
+               // printf("%lld ", numbersArray[i]);
+            }
+            if(can_be_solved(0, 1, numbersArray[0], numbersArray)){
+                //printf("\t\t%lld can be achieved.", numbersArray[0]);
+                answer += (uint64_t)numbersArray[0];
+            }
+             //putchar('\n');
              break;
             }
-        }
-       
-        
-        
-     
+        }     
     }
-    
+
+    printf("%lld\n", answer);
+    return 0;
 
 }
 
@@ -57,10 +63,10 @@ void unget_f_c(char c){
         fprintf(stderr, "Error: unfet_f_c: Buffer is full.\n");
     }
 }
-int32_t get_f_number(FILE *fp, int32_t limit, int32_t *number){
+int64_t get_f_number(FILE *fp, int32_t limit, int64_t *number){
 
     int32_t c = 0; 
-    int32_t value = 0;
+    int64_t value = 0;
 
     while ((c = get_f_c(fp)) == ' '){
         ;
@@ -74,7 +80,7 @@ int32_t get_f_number(FILE *fp, int32_t limit, int32_t *number){
 
     while(--limit ){
         if(!isdigit((c = get_f_c(fp)))){
-            //unget_f_c(c);
+            // unget_f_c(c);
             break;
         }
         value = value * 10 + c - '0';
@@ -83,4 +89,18 @@ int32_t get_f_number(FILE *fp, int32_t limit, int32_t *number){
     *number = value;
     return c;
     
+}
+
+int32_t can_be_solved(int64_t acc, int32_t index, int64_t answer, int64_t *numbers){
+    if(numbers[index] == -1){
+        if(acc == answer){
+            return 1;
+        }
+        return 0;
+    }
+    index++;
+    if(can_be_solved(acc + numbers[index-1], index, answer, numbers)){
+        return 1;
+    }
+    return can_be_solved(acc * numbers[index-1], index, answer, numbers);
 }
